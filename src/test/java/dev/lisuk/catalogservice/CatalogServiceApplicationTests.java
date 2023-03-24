ac118beb9cd079b1ec47698307008641a4eb503b
David Lisuk <david@lisuk.dev>
2023-03-24 17:02:10 -0400

    Initializing initial schema using flywheel

--- src/test/java/dev/lisuk/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/dev/lisuk/catalogservice/CatalogServiceApplicationTests.java
@@ -4,13 +4,15 @@ import dev.lisuk.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
